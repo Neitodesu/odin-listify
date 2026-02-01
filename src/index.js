8b8ed7706f576c0f479219e88d46fc08b1a35113
Neitodesu <95891209+Neitodesu@users.noreply.github.com>
2026-02-01 12:12:26 -0500

    added add todo modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './styles.css';
+
+const newTodoBtn = document.querySelector('#addItemBtn');
+const todoModal = document.querySelector('#newTodoModal');
+const todoModalBackdrop = document.querySelector('.todo-modal-backdrop');
+const todoModalDetails = document.querySelector('.todo-modal-container');
+
+todoModalDetails.addEventListener('click', (e) => {
+  e.stopPropagation();
+});
+
+newTodoBtn.addEventListener('click', () => {
+  todoModal.showModal();
+});
+
+todoModalBackdrop.addEventListener('click', () => {
+  todoModal.close();
+});
